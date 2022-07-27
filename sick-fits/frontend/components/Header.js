75f406a2fb9107222d30b7bfb00dba1f7d5cfa9d
Jeff Meyer <jcmeyer10@gmail.com>
2022-07-27 14:02:10 -0400

    finished styling header

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -25,6 +25,12 @@ const HeaderStyles = styled.header`
         justify-content: space-between;
         align-items: center;
     }
+
+    .sub-bar {
+        display:grid;
+        grid-template-columns: 1fr auto;
+        border-bottom: 1px solid var(--black, black);
+    }
 `
 
 function Header() {
